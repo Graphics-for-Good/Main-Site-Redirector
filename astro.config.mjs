aa84e4e576436159cecb5f5aa702c5110b7bf59c
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-03-14 20:01:39 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import fetchPaths from './fetchPaths.js'
-
-// fetchPaths()
-
 // https://astro.build/config
 export default defineConfig({});
